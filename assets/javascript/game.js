5477d98df83875cbb16a270e4699eb455168c14c
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-06-09 13:18:54 -0700

    updated reset.css page and defined js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Define Variables:
+var wins=0; //Wins should start at 0 and increase by 1 after each win
+var playerGuess;    //Need to capture keys pushed
+var incorrectLetters []; //Need to push incorrectly guessed letters to this array
+var wordOptions = ['hello', 'game', 'javascript']; //Array of words that will be randomly selected for game 
+var numOfGuessesRemaining = 10; //Allow 10 incorrect letter guess in each game
+
+
+//Functions:
+
